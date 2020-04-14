b5f2deeceb06ee75f97fb41e1b3d7ed280fabcdb
Mihail Katkov <eegoo@yandex.ru>
2020-04-14 14:00:08 +0600

    add point in admin panel

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+import movies.models as movies_model
 
 # Register your models here.
+admin.site.register(movies_model.Category)
+
+admin.site.register(movies_model.Genre)
+admin.site.register(movies_model.Movie)
+admin.site.register(movies_model.movie_shots)
+admin.site.register(movies_model.Rating)
+admin.site.register(movies_model.RatingStar)
+
+admin.site.register(movies_model.Actor)
+admin.site.register(movies_model.Reviews)
