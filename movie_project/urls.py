bdb67ac118d9c60f25f131bcd38fe4e2154a96b5
Mihail Katkov <eegoo@yandex.ru>
2020-04-22 11:26:00 +0600

    edit path url movie

--- movie_project/urls.py
+++ movie_project/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('movies.urls'))
 ]
 
 if settings.DEBUG:
