88abf5e787e5ccbf50e3625307bcd4fee1646f0c
Mihail Katkov <eegoo@yandex.ru>
2020-04-22 11:28:40 +0600

    add template end view

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Movie
+from django.views.generic import View, DetailView
+
 
 # Create your views here.
+class List_Movie(View):
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, 'movies/movie_list.html', {'movies': movies})
